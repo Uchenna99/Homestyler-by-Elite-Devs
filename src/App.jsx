054b178d0d1278da3d5d8c6fc5db8a63882ae039
BiGuyBryan <paulbrian535@gmail.com>
2024-09-18 12:44:21 +0800

    Add

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import { useState } from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './Pages/HomePage'
+import About from './Pages/About'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Routes>
+      <About/>
+      {/* <Routes>
           <Route path='/' element={<HomePage/>} />
+          <Route path='/about' element={<About/>}/>
 
-      </Routes>
+      </Routes> */}
     </>
   )
 }
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import '../Stylesheets/About.css'
 import '../Stylesheets/Aboutresponsiveness.css'
-import Navbar from "./Navbar";
+// import Navbar from "./Navbar";
 
 const About =()=>{
     return(
         <>
-        <Navbar/>
+        {/* <Navbar/> */}
             <div className="aboutContainer">
                 <div className="whatWeDo">
                     <h1>WHAT WE DO</h1>
                     <h2 className="strategy">STRATEGY, MARKETING AND SUPPORT</h2>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis alias quasi sapiente qui sequi aperiam recusandae asperiores aut nobis eveniet rem modi optio expedita, ad nostrum voluptatibus eaque ducimus at!</p>
+                    <p>We're passionate about providing an exceptional online shopping experience. Our mission is to deliver high-quality products, outstanding customer service, and unbeatable prices to our valued customers.
+                    </p>
                 </div>
                 <div className="development">
                     <div className="design">
@@ -56,35 +57,35 @@ const About =()=>{
                     <div className="frame2">
                         <img src="src/assets/plan.png" alt="" />
                         <div className="frameTexts">
-                            <h6>Paulinus Okonkwo</h6>
+                            <h6>Tolulope Akinwande</h6>
                             <p>Developer</p>
                         </div>
                     </div>
                     <div className="frame1">
                         <img src="src/assets/organize.png" alt="" />
                         <div className="frameTexts">
-                            <h6>Paulinus Okonkwo</h6>
+                            <h6>Agbu Uchenna</h6>
                             <p>Developer</p>
                         </div>
                     </div>
                     <div className="frame1">
                         <img src="src/assets/organize.png" alt="" />
                         <div className="frameTexts">
-                            <h6>Paulinus Okonkwo</h6>
+                            <h6>Ejike Justice</h6>
                             <p>Developer</p>
                         </div>
                     </div>
                     <div className="frame2">
                         <img src="src/assets/plan.png" alt="" />
                         <div className="frameTexts">
-                            <h6>Paulinus Okonkwo</h6>
+                            <h6>Lewis Stephanie</h6>
                             <p>Developer</p>
                         </div>
                     </div>
                     <div className="frame1">
                         <img src="src/assets/organize.png" alt="" />
                         <div className="frameTexts">
-                            <h6>Paulinus Okonkwo</h6>
+                            <h6>Morrison Justice</h6>
                             <p>Developer</p>
                         </div>
                     </div>
