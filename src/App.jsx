5acd828c1046c0ff669b2a5fddb31f893781929d
BiGuyBryan <paulbrian535@gmail.com>
2024-09-19 13:22:56 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './Pages/HomePage'
 import About from './Pages/About'
+import FeedbackPage from './Pages/feedbackpage'
 
 function App() {
 
   return (
     <>
-      <Routes>
+      <FeedbackPage/>
+      {/* <Routes>
           <Route path='/' element={<HomePage/>} />
           <Route path='/about' element={<About/>}/>
-      </Routes>
+
+      </Routes> */}
     </>
   )
 }
---
+++ src/Pages/feedbackpage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../Stylesheets/feedbackpage.css'
+
+
+const FeedbackPage =()=>{
+    return(
+        <>
+                <div className="feedbackContainer">
+                    <div className="feedbackBox"></div>
+                </div>
+        </>
+    )
+}
+export default FeedbackPage;
