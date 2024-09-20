8d7c61face63f3cc95e7f570c10172d00e59a650
Uchenna99 <uchennagbu@gmail.com>
2024-09-20 14:37:58 +0100

    Update

--- src/Pages/shop.jsx
+++ src/Pages/shop.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import DesignStyles from "../designStyles";
 import '../Stylesheets/shop.css'
-import Navbar from "./Navbar";
+import Navbar from "../Components/Navbar";
 
 const Shop=()=>{
     return(
