889abe31bbdd6b5a9207c4a79ca0d64fb0acde19
Uchenna99 <uchennaagbu@gmail.com>
2025-06-24 13:24:54 +0100

    adjusting navbar logic

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,13 +6,14 @@ import { IoCartOutline } from "react-icons/io5";
 import { FiSearch } from "react-icons/fi";
 import { AiOutlineClose } from "react-icons/ai";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { Menu, X } from "lucide-react";
 
 const Navbar =()=>{
-    const [buttonOpen, setButtonOpen] = useState(true);
+    const [buttonOpen, setButtonOpen] = useState(false);
 
     const toggle = () => {
-        setButtonOpen(!buttonOpen)
-      
+        if(buttonOpen) { setButtonOpen(false) }
+        else{ setButtonOpen(true) }
     }
 
     return(
@@ -38,31 +39,25 @@ const Navbar =()=>{
                 </div>
             </div>
 
-            {/* <div className="nav-right">
-                <div className="nav-right-wrap">
-                    <FiSearch className="search-icon" />
-                    <IoCartOutline className="cart-icon" />
-                </div>
-            </div> */}
 
-            <div className="nav-burger">
-                {/* <RxHamburgerMenu className='burger-icon' onClick={toggle}/> */}
-                {/* <AiOutlineClose className="burger-close" onClick={toggle}/> */}
 
-             
-                <input type="checkbox" id="checkbox" onClick={toggle}/>
-                    <label htmlFor="checkbox" class="toggle">
-                        <div className="bars" id="bar1"></div>
-                        <div className="bars" id="bar2"></div>
-                        <div className="bars" id="bar3"></div>
-                    </label>
+            <div className="nav-burger" tabIndex={-1} onClick={toggle}
+                onBlur={()=> setButtonOpen(false)}>
 
-                {!buttonOpen && (
+                {
+                    !buttonOpen?
+                    <Menu size={30} color="#2D2D2D" />
+                    :
+                    <X size={30} color="#2D2D2D" />
+                }
+                
+
+                {buttonOpen && (
                     <div className="nav-burger-drop-menu">
                         <NavLink to='/' id='drop-link'>
-                        <div className="drop-menu-option">
-                        <p>Home</p>
-                        </div>
+                            <div className="drop-menu-option">
+                            <p>Home</p>
+                            </div>
                         </NavLink>
                         <NavLink to='/shop' id='drop-link'>
                         <div className="drop-menu-option">
