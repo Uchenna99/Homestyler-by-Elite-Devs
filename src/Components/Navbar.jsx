87779e4a080721d8c4a22ec73a8593e0d82d9544
Uchenna99 <uchennaagbu@gmail.com>
2025-06-27 23:43:39 +0100

    retouching shop section and responsiveness

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,14 +4,14 @@ import { Link, NavLink } from "react-router-dom";
 import { Menu, X } from "lucide-react";
 import { motion } from "framer-motion";
 
-const Navbar =()=>{
+const Navbar =({ page })=>{
     const [buttonOpen, setButtonOpen] = useState(false);
     const [scrolled, setScrolled] = useState(false);
 
     useEffect(() => {
       const handleScroll = () => {
         const scrollPosition = window.scrollY;
-        const triggerPoint = 400;
+        const triggerPoint = page === 'home'? 400 : 50
 
         if (scrollPosition > triggerPoint) {
           setScrolled(true);
@@ -43,20 +43,32 @@ const Navbar =()=>{
         <div className={`navbar transition-all duration-300 ${scrolled? 'bg-white shadow-sm' : 'bg-transparent'}`}>
             <div className="nav-left">
                 <Link to='/'>
-                    <p className="logoText">Home Styler</p>
+                    <p className="logoText" style={{color: scrolled?'#2D2D2D' : 'white'}}>Home Styler</p>
                 </Link>
             </div>
 
             <div className="nav-mid">
-                <NavLink to='/' id='link'><div className="nav-option"><p>Home</p></div></NavLink>
-                <NavLink to='/shop' id='link'><div className="nav-option"><p>Shop</p></div></NavLink>
-                <NavLink to='/about' id='link'><div className="nav-option"><p>About</p></div></NavLink>
-                <NavLink to='/contact-page' id='link'><div className="nav-option"><p>Contact Us</p></div></NavLink>
+                <NavLink to='/' id='link'>
+                    <div className="nav-option"><p style={{color: scrolled?'#2D2D2D' : 'white'}}>Home</p></div>
+                </NavLink>
+                <NavLink to='/shop' id='link'>
+                    <div className="nav-option"><p style={{color: scrolled?'#2D2D2D' : 'white'}}>Shop</p></div>
+                </NavLink>
+                <NavLink to='/about' id='link'>
+                    <div className="nav-option"><p style={{color: scrolled?'#2D2D2D' : 'white'}}>About</p></div>
+                </NavLink>
+                <NavLink to='/contact-page' id='link'>
+                    <div className="nav-option"><p style={{color: scrolled?'#2D2D2D' : 'white'}}>Contact Us</p></div>
+                </NavLink>
             </div>
 
             <div className="nav-right">
                 <div className="nav-right-wrap">
-                    <Link to='/login' id='login-link'><div className="login-wrap"><p>Log In</p></div></Link>
+                    <Link to='/login' id='login-link'>
+                        <div className="login-wrap" style={{borderRight: scrolled?'1px solid #2D2D2D' : '1px solid white'}}>
+                            <p style={{color: scrolled?'#2D2D2D' : 'white'}}>Log In</p>
+                        </div>
+                    </Link>
                     <div className="signin-wrap">
                         <Link to='/signup'><button className='nav-butn'>Sign Up</button></Link>
                     </div>
